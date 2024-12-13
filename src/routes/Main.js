423eb2db5744ceda1e81850fc7681f22d1fbfc34
KrishChouhan486 <your-email@example.com>
2024-12-13 23:51:23 +0530

    final

--- src/routes/Main.js
+++ src/routes/Main.js
@@ -13,7 +13,7 @@ const Main = () => {
       <div className="App">
         {/* Routes */}
         <Routes>
-          <Route path="/Xenon_Task1_ResponsiveWebsite" element={<LandingPage />} />
+          <Route path="/rest2" element={<LandingPage />} />
           <Route path="/login-signup" element={<App/>}/>
           <Route path="/dashboard" element={<HomePage />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
